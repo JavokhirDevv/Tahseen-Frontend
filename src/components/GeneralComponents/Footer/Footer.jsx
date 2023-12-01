8f52c487ab7bb5c4536755ea25b131560d889bb6
AzizbekFullStackDev <144344579+AzizbekFullStackDev@users.noreply.github.com>
2023-12-01 15:44:17 +0500

    Footer has been changed

--- src/components/GeneralComponents/Footer/Footer.jsx
+++ src/components/GeneralComponents/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './Footer.css'
 const Footer = () => {
   return (
-    <div className='footer'>
-        <div className="footer_links_cover">
+    <div className='footer-main'>
+        {/* <div className="footer_links_cover">
         <ul>
             <li><a href="/">Email: tahseen.uz@gmail.com</a></li>
             <li><a href="/">Telefon raqam: +998945678960</a></li>
@@ -23,8 +23,29 @@ const Footer = () => {
         </ul>
 
         </div>
-        <h1>Tahseen_Team 2023</h1>
+        <h1>Tahseen_Team 2023</h1> */}
 
+        <div className="footer-line"></div>
+        <div className="footer-flex">
+          <ul className='footer-items'>
+            <li className='footer-item'>
+              <a href="#">Yordam</a>
+            </li>
+            <li className='footer-item'>
+              <a href="#">Biz haqimizda</a>
+            </li>
+            <li className='footer-item'>
+              <a href="#">Xafvsizlik</a>
+            </li>
+            <li className='footer-item'>
+              <a href="#">Bog'lanish</a>
+            </li>
+          </ul>
+
+          <div className="footer-tahseen">
+            <h1>Tahseen 2023 © </h1>
+          </div>
+        </div>
     </div>
   )
 }
